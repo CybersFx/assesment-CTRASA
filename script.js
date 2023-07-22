019b830a8df5b7b41eabd8a11c1e652faa4f884a
AjiS <cybersfx@gmail.com>
2023-07-23 00:57:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+    // JavaScript for horizontal scroll effect
+    const reviewSection = document.querySelector('.review-section');
+    let isMouseDown = false;
+    let startX, scrollLeft;
+
+    reviewSection.addEventListener('mousedown', (e) => {
+      isMouseDown = true;
+      startX = e.pageX - reviewSection.offsetLeft;
+      scrollLeft = reviewSection.scrollLeft;
+    });
+
+    reviewSection.addEventListener('mouseleave', () => {
+      isMouseDown = false;
+    });
+
+    reviewSection.addEventListener('mouseup', () => {
+      isMouseDown = false;
+    });
+
+    reviewSection.addEventListener('mousemove', (e) => {
+      if (!isMouseDown) return;
+      e.preventDefault();
+      const x = e.pageX - reviewSection.offsetLeft;
+      const walk = (x - startX) * 1.5; // You can adjust the scrolling speed here
+      reviewSection.scrollLeft = scrollLeft - walk;
+    });
+
+
+
